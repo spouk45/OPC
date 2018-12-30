b89a525882ee94f8aaf304415ac8b093cf4bebc6
spouk <yoan.beauchamp@gmail.com>
2018-12-31 00:04:05 +0100

    style dashboard et customer

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ Encore
      * and one CSS file (e.g. app.scss) if you JavaScript imports CSS.
      */
     .addEntry('app', './assets/js/app.js')
+    .addStyleEntry('map', './assets/css/map.scss')
     //.addEntry('getCoord', './assets/js/getCoord.js')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
