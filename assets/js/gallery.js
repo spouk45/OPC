93d3a07d19533b4c6d2757f57b9287c2df6f1fb5
spouk <yoan.beauchamp@gmail.com>
2019-01-20 22:47:01 +0100

    delete img view

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,19 +1,67 @@
 // Get the modal
 let modal = $('#myModal');
-
+let selectorActivated = false;
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 let modalImg = $("#img01");
 let captionText = $("#caption");
-$('.myImg').click(function(){
-    modal.css('display','block');
-    modalImg.attr('src',$(this).attr('src'));
-    captionText.html($(this).attr('alt'));
+let selectedImgId = [];
+
+$('.myImg').click(function () {
+    if (!selectorActivated) {
+        modal.css('display', 'block');
+        modalImg.attr('src', $(this).attr('src'));
+        captionText.html($(this).attr('alt'));
+    } else {
+        $(this).toggleClass('active');
+    }
 });
 
 // Get the <span> element that closes the modal
 let span = $('#closeModal');
 
 // When the user clicks on <span> (x), close the modal
-span.click(function(){
-    modal.css('display','none');
+span.click(function () {
+    modal.css('display', 'none');
+});
+
+// selection mode
+$('#selector').click(function () {
+    $(this).toggleClass('active');
+    if ($(this).hasClass('active')) {
+        selectorActivated = true;
+        $('#deleteImg').show();
+    } else {
+        selectorActivated = false;
+        $('.myImg').removeClass('active');
+        $('#deleteImg').hide();
+        console.log(selectedImgId);
+    }
+
+});
+
+$('#deleteImg').click(function () {
+    let tab = $('.myImg.active');
+    if(tab.length > 0 ){
+        $('#deleteModal').show();
+    }
+    // récupération de la liste des image id selectionnées
+    tab.each(function(){
+         let id = $(this).attr('data-id');
+        selectedImgId.push(id);
+    });
+
+    // open confirmation modal
+
+    // process post delete
+
+    tab.each(function () {
+        $(this).parents('.blockImg').remove();
+    });
+    selectedImgId = [];
+
+});
+
+
+$('#deleteModal span.close').click(function(){
+    $('#deleteModal').hide();
 });
